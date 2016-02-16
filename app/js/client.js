4b146e35f5d20ea9243b47f2f7fe3a1b1ac04d62
SamHeutmaker <samheutmaker@gmail.com>
2016-02-15 17:31:36 -0800

    Complete

--- app/js/client.js
+++ app/js/client.js
@@ -4,7 +4,9 @@ const ReactDOM = require('react-dom');
 var GetBears = React.createClass({
   getInitialState: function() {
     this.displayBears();
-    return { bears: [] };
+    return {
+      bears: []
+    };
   },
   editBear: function(id) {
     return () => {
@@ -14,7 +16,29 @@ var GetBears = React.createClass({
         }
         return bear;
       });
-      this.setState({ bears: holder });
+      this.setState({
+        bears: holder
+      });
+    }
+  },
+  removeBear: function(id) {
+    return () => {
+      var holder = this.state.bears.filter(function(bear) {
+        if(bear._id === id) {
+          return false
+        } else {
+         return true;
+        }
+      });
+
+      this.setState({bears: holder});
+
+      $.ajax({
+        type:'DELETE',
+        url: 'http://localhost:3000/api/bears/' + id
+      }).then(function(data) {
+        console.log(data);
+      })
     }
   },
   saveBear: function(event) {
@@ -42,7 +66,9 @@ var GetBears = React.createClass({
       }
       return bear;
     });
-    this.setState({ bears: holder });
+    this.setState({
+      bears: holder
+    });
   },
   displayBears: function() {
     $.ajax({
@@ -52,58 +78,92 @@ var GetBears = React.createClass({
       res.forEach(function(bear, i) {
         bear.editing = false;
       });
-      this.setState({ bears: res });
+      this.setState({
+        bears: res
+      });
     });
   },
   render: function() {
-    return (
-      <ul>
-        {this.state.bears.map((bear) => {
-          return <li key={bear._id}>{bear.name}, {bear.flavor}, {bear.fishPreference}
-          <button onClick={this.editBear(bear._id)}>EDIT</button>
-          <button>DELETE</button>
-          <form id={bear._id} className={bear.editing ? null : "hidden"} onSubmit={this.saveBear}>
-            <input type="text" name="bear-name" placeholder="Bear Name" defaultValue={bear.name}/>
-            <input type="text" name="bear-flavor" placeholder="Bear Flavor" defaultValue={bear.flavor}/>
-            <input type="text" name="bear-fish-preference" placeholder="Bear Fish Preference" defaultValue={bear.fishPreference}/>
-            <button type="submit">SAVE BEAR</button>
-          </form>
-          </li>
-        })}
-      </ul>
-    )
+    return ( < ul > {
+      this.state.bears.map((bear) => {
+        return <li key = {
+          bear._id
+        } > {
+          bear.name
+        }, {
+          bear.flavor
+        }, {
+          bear.fishPreference
+        } < button onClick = {
+          this.editBear(bear._id)
+        } > EDIT < /button> < button onClick={this.removeBear(bear._id)}> DELETE < /button> < form id = {
+          bear._id
+        }
+        className = {
+          bear.editing ? null : "hidden"
+        }
+        onSubmit = {
+            this.saveBear
+          } >
+          < input type = "text"
+        name = "bear-name"
+        placeholder = "Bear Name"
+        defaultValue = {
+          bear.name
+        }
+        /> < input type = "text"
+        name = "bear-flavor"
+        placeholder = "Bear Flavor"
+        defaultValue = {
+          bear.flavor
+        }
+        /> < input type = "text"
+        name = "bear-fish-preference"
+        placeholder = "Bear Fish Preference"
+        defaultValue = {
+          bear.fishPreference
+        }
+        /> < button type = "submit" > SAVE BEAR < /button> <
+          /form> < /li>
+      })
+    } < /ul>)
   }
 });
 
 var NewBear = React.createClass({
   createBear: function(event) {
     event.preventDefault();
+
     var bearData = {
       name: event.target.children['bear-name'].value,
       flavor: event.target.children['bear-flavor'].value,
       fishPreference: event.target.children['bear-fish-preference'].value
-    }
-    $.ajax({
-      url: 'http://localhost:3000/api/bears',
-      type: 'POST',
-      data: bearData
-    }).then(
-      function(data) {
-        console.log(data);
-      }
-    );
+    };
+    
+    console.log(bearData);
+
+    $.post('http://localhost:3000/api/bears', bearData, function(data) {
+      console.log(data);
+    })
+
   },
   render: function() {
-    return (
-      <form onSubmit={this.createBear}>
-        <input type="text" name="bear-name" placeholder="Bear Name" />
-        <input type="text" name="bear-flavor" placeholder="Bear Flavor" />
-        <input type="text" name="bear-fish-preference" placeholder="Bear Fish Preference" />
-        <button type="submit">CREATE BEAR</button>
-      </form>
+    return ( < form onSubmit = {
+        this.createBear
+      } >
+      < input type = "text"
+      name = "bear-name"
+      placeholder = "Bear Name" / >
+      < input type = "text"
+      name = "bear-flavor"
+      placeholder = "Bear Flavor" / >
+      < input type = "text"
+      name = "bear-fish-preference"
+      placeholder = "Bear Fish Preference" / >
+      < button type = "submit" > CREATE BEAR < /button> < /form>
     )
   }
 });
 
-ReactDOM.render(<NewBear />, document.getElementById('newbear'));
-ReactDOM.render(<GetBears />, document.getElementById('bear-container'));
+ReactDOM.render( < NewBear / > , document.getElementById('newbear'));
+ReactDOM.render( < GetBears / > , document.getElementById('bear-container'));
